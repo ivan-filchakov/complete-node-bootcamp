48b7f9e30a0b92d756006c1ba0440cd8e59d1cde
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-02-07 22:52:16 +0200

    connect mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,16 @@
-const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
-const port = parseInt(process.env.PORT);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => console.log('DB connection successfull'));
+
+const port = +process.env.PORT;
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
 });
