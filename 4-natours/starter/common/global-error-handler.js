c07d91be4d8ba7ba87f721b0efc94a4e33872c57
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-09-08 04:49:23 +0300

    cast error

--- 4-natours/starter/common/global-error-handler.js
+++ 4-natours/starter/common/global-error-handler.js
@@ -1,12 +1,58 @@
-function globalErrorHandler(err, req, res, next) {
-  // eslint-disable-next-line no-console
-  console.error(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+/* eslint-disable no-unused-vars */
+const AppError = require('./app-error');
+
+function sendErrorDev(err, req, res) {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+}
+
+function sendErrorProd(err, req, res) {
+  // OPERATIONAL ERROR WE TRUST AND SEND TO CLIENT
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // PROGRAMING ERROR OR OTHER UNKNOWN SHIT
+    // eslint-disable-next-line no-console
+    console.error(err);
+    // GENERIC MESSAGE
+    res.status(500).json({
+      status: 'error',
+      message: 'oops! something went wrong!',
+    });
+  }
+}
+
+function handleCastErrorDB(err) {
+  return new AppError({
+    statusCode: 400,
+    message: `Invalid ${err.path}: ${err.value}`,
+  });
+}
+
+function globalErrorHandler(err, req, res, next) {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+    return;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    sendErrorProd(error, req, res);
+  }
 }
 
 module.exports = globalErrorHandler;
