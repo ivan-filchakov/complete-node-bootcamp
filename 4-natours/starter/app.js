abef259f346a11add6e5724680afaea4f1a1bb4a
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-09-09 01:07:07 +0300

    refactor appjs

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -6,20 +7,20 @@ const AppError = require('./common/app-error');
 const globalErrorHandler = require('./common/global-error-handler');
 
 const app = express();
-// eslint-disable-next-line no-console
 console.info({ development_mode: process.env.NODE_ENV });
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); /* HTTP request logger */
 }
 
 // MIDDLEWARE
-
 app.use(express.json()); /* parse body */
 
-app.use((req, res, next) => {
+function globalMiddleware(req, res, next) {
   req.requestTime = new Date().toISOString();
+  console.log('request time:', req.requestTime);
   next();
-});
+}
+app.use(globalMiddleware);
 
 // app.use(express.static(`${__dirname}/public`)); /* static files access */
 
@@ -28,13 +29,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 function handleBadRoute(req, res, next) {
-  next(
-    new AppError({
-      message: `bad url: ${req.originalUrl}`,
-      status: 'fail',
-      statusCode: 404,
-    }),
-  );
+  throw new AppError({
+    message: `bad url: ${req.originalUrl}`,
+    status: 'fail',
+    statusCode: 404,
+  });
 }
 app.all('*', handleBadRoute);
 
