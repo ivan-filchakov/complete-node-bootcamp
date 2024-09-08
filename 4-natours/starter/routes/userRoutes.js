874aaa69f6e5a4c8bb97c81b7ef88d7ede257492
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-09-08 19:38:46 +0300

    signup

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -6,8 +6,11 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
