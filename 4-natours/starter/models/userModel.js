ae3fcfdaee5ac16c5b8aeae19eddb18db9feb5a3
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-09-08 20:13:31 +0300

    cleanup

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const catchAsync = require('../common/catch-async');
 
 const userSchema = new mongoose.Schema(
   {
