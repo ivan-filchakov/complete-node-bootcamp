503465644ecd10941d7546a970d2fcc0e548c0e4
Ivan Filchakov <ivan.filchakov@gmail.com>
2024-09-08 20:15:25 +0300

    role protection fix

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsync = require('../common/catch-async');
 
 module.exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
   res.status(201).json({
     status: 'success',
     data: {
